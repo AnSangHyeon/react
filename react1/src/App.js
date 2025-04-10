6d40f723757f2fed3b0eabe1c4b83fb94e47aeb6
AnSangHyeon <ash040906@naver.com>
2025-04-10 14:11:06 +0900

    오늘 첫 커밋

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
 
 function Square() {
-  <>
-    <div className="board-row">
-      <button>X</button>
-      <button>X</button>
-      <button>X</button>
-    </div>
-  </>
+  return(
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
 }
 
 export default function App() {
   return (
     <>
+      <h1>Tic Tac Toe</h1>
       <Square />
     </>
   )
